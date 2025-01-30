282be2b5c2fcb83c528dc7a0f9814e0abbc6790b
machinegunbryce <brchan1@s.sfusd.edu>
2025-01-29 21:25:21 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,6 @@ public class Main {
 	
 	public static void main(final String[] args) {
 		PigLatin sim = new PigLatin();
-
-   if(word.substring(0,1).equals("a") || word.substring(0,1).equals("e") || 
-    word.substring(0,1).equals("i") || word.substring(0,1).equals("o") || 
-    word.substring(0,1).equals("u")){
-
-  
-
         	//sim.tester();
 	}
 	
